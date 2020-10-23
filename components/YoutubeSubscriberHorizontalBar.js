7f917357756dcf1ff3f79bddfb277959baa672bb
Nokz <dreamwithnokz@gmail.com>
2020-10-24 07:05:36 +0800

    Design the horizontal bar component

--- components/YoutubeSubscriberHorizontalBar.js
+++ components/YoutubeSubscriberHorizontalBar.js
@@ -2,24 +2,42 @@ import React from "react";
 import { HorizontalBar } from "react-chartjs-2";
 
 const data = {
-  labels: ["January", "February", "March", "April", "May", "June"],
+  labels: [
+    "as",
+    "asd",
+    "asdd",
+    "asd",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+  ],
   datasets: [
     {
-      label: "Dataset",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgb(11,227,210)",
-      borderWidth: 1,
+      data: [14, 20, 30, 65, 59, 80, 81, 56, 55, 40],
+      label: "Subscribers",
+      backgroundColor: "rgba(244,105,105,0.5)",
       hoverBackgroundColor: "rgba(255,0,54,0.4)",
       hoverBorderColor: "rgb(0,88,101)",
-      data: [65, 59, 80, 81, 56, 55, 40],
     },
   ],
 };
 
-const YoutubeSubscriberHorizontalBar = () => (
-  <div>
-    <HorizontalBar data={data} width={400} height={400} />
-  </div>
-);
+const options = {
+  legend: {
+    position: "bottom",
+    onClick: (e) => e.stopPropagation(),
+  },
+};
 
-export default YoutubeSubscriberHorizontalBar;
+export default class YoutubeSubscriberHorizontalBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <HorizontalBar data={data} options={options} width={400} height={300} />
+      </div>
+    );
+  }
+}
